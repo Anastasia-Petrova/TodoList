6319cec27d30868a275b1f6860d474df29433778
Anastasia Petrova <anastasia.iosdev@gmail.com>
2019-07-14 12:19:06 +0100

    move init to extension

--- NewCheckList/TodoItemViewModel.swift
+++ NewCheckList/TodoItemViewModel.swift
@@ -1,11 +1,13 @@
-struct TodoItemViewModel: CoreDataMappable {
+struct TodoItemViewModel  {
+    let text: String
+    let isChecked: Bool
+}
+
+extension TodoItemViewModel: CoreDataMappable {
     typealias CoreDataModel = TodoItem
     
     init(model: TodoItem) {
         text = model.name ?? ""
         isChecked = false //TODO: Add isChecked to DB
     }
-    
-    let text: String
-    let isChecked: Bool
 }
